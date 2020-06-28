d4f728c62c93d29d61d39db15142de1ba278e133
Renato Miranda <re.miranda@outlook.com.br>
2020-06-28 13:46:33 -0300

    fix

--- push.js
+++ push.js
@@ -7,6 +7,12 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:ree.arua@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {}
+let sub = {
+    endpoint: 'https://fcm.googleapis.com/fcm/send/dhNEATEws-w:APA91bGHPJaNnmxZqVOhqOqHOQLfVPOEi1GY5WtaiNOC7jansqttqqjK2zNiBrFhtqw2q0HS0Gyj3d0RIMnF41tCNPj9sFOi3wzizdN4PzZ6vyx-zRPWA0jXbL3Hz6RX53eBtSic3dyP","expirationTime":null',
+    keys: {
+        p256dh: 'BBmiM4D99_ngCYzRLQyD1PHWORI80IQ0I3sfp8AF5OPgbzidfCL_yXZo6G3OoaR5CZ36X4j52U-HzmWfxLj6Afk',
+        auth: 'CtO1_Wza6MNK0uAGy9T9eg'
+    }
+};
 
 push.sendNotification(sub, 'Teste Mensagem!');
